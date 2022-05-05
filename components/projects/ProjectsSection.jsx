68757a032837ab060f0f9c094fa7cc0bed66bb15
anasmasti <anasmasti@hotmail.com>
2022-05-05 15:05:25 +0000

    Add projects components

---
+++ components/projects/ProjectsSection.jsx
@@ -0,0 +1,9 @@
+import MainTitle from "../shared/MainTitle";
+
+export default function ProjectsSection() {
+  return (
+    <section className="bg-black text-white p-10 lg:p-24 mt-10 md:flex md:justify-center md:items-center md:flex-col lg:flex lg:justify-center lg:items-center lg:flex-col">
+      <MainTitle title="Some projects" />
+    </section>
+  );
+}
--- components/skills/SkillsSection.jsx
+++ components/skills/SkillsSection.jsx
@@ -3,8 +3,9 @@ import MainTitle from '../shared/MainTitle'
 
 export default function SkillsSection() {
   return (
-      <section>
+      <section className="bg-black text-white p-10 lg:p-24 mt-10 md:flex md:justify-center md:items-center md:flex-col lg:flex lg:justify-center lg:items-center lg:flex-col">
           <MainTitle title='Skills' />
+          
       </section>
   )
 }
