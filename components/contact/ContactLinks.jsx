65dd1a10cefaa22c74b921935dfded2d5d9392e4
anasmasti <anasmasti@hotmail.com>
2022-05-05 10:29:03 +0000

    Add Contact links

---
+++ components/contact/ContactLinks.jsx
@@ -0,0 +1,16 @@
+
+export default function ContactLinks() {
+  return (
+    <div className="flex justify-around mt-10">
+    <a href="" className="bg-pink-400">
+      Messenger
+    </a>
+    <a href="" className="bg-green-400">
+      WhatsApp
+    </a>
+    <a href="" className="bg-blue-400">
+      Telegram
+    </a>
+  </div>
+  )
+}
--- components/contact/ContactSection.jsx
+++ components/contact/ContactSection.jsx
@@ -1,12 +1,13 @@
 import MainTitle from "../shared/MainTitle";
 import ContactForm from "./ContactForm";
+import ContactLinks from "./ContactLinks";
 
 export default function ContactSection() {
   return (
-    <div className="bg-black text-white p-10 lg:p-24 mt-10 md:flex md:justify-center md:items-center md:flex-col lg:flex lg:justify-center lg:items-center lg:flex-col">
-
-        <MainTitle title='Contact' />
-        <ContactForm />
-    </div>
-  )
+    <section className="bg-black text-white p-10 lg:p-24 mt-10 md:flex md:justify-center md:items-center md:flex-col lg:flex lg:justify-center lg:items-center lg:flex-col">
+      <MainTitle title="Contact" />
+      <ContactLinks />
+      <ContactForm />
+    </section>
+  );
 }
