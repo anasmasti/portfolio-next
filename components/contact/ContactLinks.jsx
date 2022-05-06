c0caa48f5ad4aaaccdb6ba9bb457726eb36785bc
anasmasti <anasmasti@hotmail.com>
2022-05-06 16:16:28 +0000

    Fix contact bg images issues

--- components/contact/ContactLinks.jsx
+++ components/contact/ContactLinks.jsx
@@ -6,31 +6,36 @@ export default function ContactLinks() {
       id: 1,
       title: "WhatsApp",
       url: `https://wa.me/212660548100?text=${message}`,
-      bg_img: "",
+      bg_img: "bg-whatsapp",
     },
     {
       id: 2,
       title: "Messenger",
       url: "https://m.me/anas.masti.96",
-      bg_img: "",
+      bg_img: "bg-messenger",
     },
-    { id: 3, title: "Telegram", url: "https://t.me/anasmasti", bg_img: "" },
+    {
+      id: 3,
+      title: "Telegram",
+      url: "https://t.me/anasmasti",
+      bg_img: "bg-telegram",
+    },
     {
       id: 4,
       title: "E-mail",
       url: `mailto:anasmasti@hotmail.com?subject='Anas Masti Portfolio' Contact&body=${message}`,
-      bg_img: "",
+      bg_img: "bg-mail",
     },
   ];
 
   return (
-    <div className="flex justify-around mt-10">
+    <div className="flex justify-between gap-3 mt-10">
       {links.map((link) => (
         <a
-          className="p-5 bg-cover bg-no-repeat bg-center bg-whatsapp rounded-xl"
           key={link.id}
-          target="_blank"
           href={link.url}
+          target="_blank"
+          className={`p-3 bg-cover bg-no-repeat bg-center ${link.bg_img} rounded-xl`}
         >
           {link.title}
         </a>
