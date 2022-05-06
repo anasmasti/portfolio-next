cbef3cc846329e15c506cdb92e7c5be4e8cffa34
anasmasti <anasmasti@hotmail.com>
2022-05-06 14:59:25 +0000

    Add Skills section with list

--- components/layouts/PagesLayout.jsx
+++ components/layouts/PagesLayout.jsx
@@ -4,7 +4,7 @@ import GoHomeButton from "../shared/GoHomeButton";
 export default function PagesLayout({ children }) {
   return (
     <>
-      <div className="w-full flex flex-row justify-start items-start">
+      <div className="w-full flex flex-row justify-start items-start p-6">
         <GoHomeButton />
       </div>
       <section>{children}</section>
