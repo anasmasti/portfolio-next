abfcefe3bfa4f91d342eee204c28cced067777f7
anasmasti <anasmasti@hotmail.com>
2022-05-09 10:11:43 +0100

    Fix skills UI issue

--- components/skills/SkillsSection.jsx
+++ components/skills/SkillsSection.jsx
@@ -5,7 +5,7 @@ import SkillsItems from "./SkillsItems";
 export default function SkillsSection() {
   return (
     <PagesLayout>
-      <div className="bg-black text-white p-10 lg:p-24 mt-10 md:flex md:justify-center md:items-center md:flex-col lg:flex lg:justify-center lg:items-center lg:flex-col">
+      <div className="bg-black text-white p-10 lg:p-10 md:flex md:justify-center md:items-center md:flex-col lg:flex lg:justify-center lg:items-center lg:flex-col">
         <div className="flex flex-col lg:w-1/2 md:w-full">
           <MainTitle title="Skills" />
         </div>
