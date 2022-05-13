014f1454b4105aaf3ffa7dcae9ce0cc0d528a0e2
anasmasti <anasmasti@hotmail.com>
2022-05-13 10:12:41 +0100

    Add projects bg

--- components/projects/ProjectItem.jsx
+++ components/projects/ProjectItem.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-export default function ProjectItem({ title, link, tech }) {
+export default function ProjectItem({ title, link, tech, bg }) {
   const openModal = (projectId) => {
     console.log(projectId);
   };
 
   return (
     <div
-      className={`p-3 my-3 bg-cover bg-no-repeat bg-center bg-mediexperts-academy rounded-xl`}
+      className={`p-3 my-3 bg-cover bg-no-repeat bg-center ${bg} rounded-xl`}
     >
       <a
         href={link}
         target="_blank"
-        id="mediexperts_academy"
-        onClick={openModal("mediexperts_academy")}
       >
         <h3 className="text-3xl font-bold">{title}</h3>
         <pre className="mt-5">{tech}</pre>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,17 @@ module.exports = {
         mail: "linear-gradient(#0d023e2e,#000000d4),url('/images/contact/mail.webp')",
         telegram:
           "linear-gradient(#0d023e2e,#000000d4),url('/images/contact/telegram.webp')",
-
-          'mediexperts-academy': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/mediexperts-academy.webp')"
+        'mediexperts-academy': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/mediexperts-academy.webp')",
+        'coastalwest-limousines': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/coastalwest-limousines.webp')",
+        'copra': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/copra.webp')",
+        'hijaby': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/hijaby.webp')",
+        'matiplas': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/matiplas.webp')",
+        'm-global': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/m-global.webp')",
+        'mediexperts': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/mediexperts.webp')",
+        'ps': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/ps.webp')",
+        'mysys': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/mysys.webp')",
+        'old': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/old.webp')",
+        'mc': "linear-gradient(#0d023e2e,#000000d4),url('/images/projects/mc.webp')",
       },
     },
   },
