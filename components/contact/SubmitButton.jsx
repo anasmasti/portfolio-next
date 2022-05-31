6873acf2ff33ac5aac55371a7591eadfbc69aba0
anasmasti <anasmasti@hotmail.com>
2022-05-31 12:55:59 +0100

    Add env file

--- components/contact/SubmitButton.jsx
+++ components/contact/SubmitButton.jsx
@@ -1,19 +1,23 @@
 import { useContext } from "react";
 import { contactFormContext } from "./ContactFormContext";
+import sendMessage from "../../services/contact/sendMeddase";
 
 export default function SubmitButton() {
-  const { formData} = useContext(contactFormContext);
+  const { formData } = useContext(contactFormContext);
 
-  let handleSubmit = (event) => {
+  let handleSubmit = () => {
     event.preventDefault();
-    console.log(formData);
+    // convert data to json for submit it
+    let dataToJson = JSON.stringify(formData);
+    // Send message to database
+    sendMessage(dataToJson);
   };
 
   return (
     <button
       type="submit"
       className="p-3 bg-black rounded-xl text-white font-bold flex justify-center gap-1 transition-all duration-300 delay-75 hover:bg-[#341e95]"
-      onClick={() => handleSubmit(event)}
+      onClick={() => handleSubmit()}
     >
       Send
     </button>
