6b8d0f7277054c08237d5a980ad908a39250a5f1
anasmasti <anasmasti@hotmail.com>
2022-07-09 21:01:33 +0100

    Add landing section

---
+++ components/landing/LandingSection.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from "react";
+import GLOBE from "vanta/dist/vanta.globe.min";
+import Menu from "./../Menu/Menu";
+import LandingTitle from "./LandingTitle";
+
+export default function LandingSection() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const landingRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        GLOBE({
+          el: landingRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x823fff,
+          backgroundColor: 0x0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+  return (
+    <>
+      <div ref={landingRef} className="h-screen z-0 absolute"></div>
+      <div className="h-screen bg-transparent flex justify-center items-center p-5 flex-col z-10 relative">
+        <LandingTitle />
+        <div className="mt-4">
+          <Menu />
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import AboutMeSection from "../components/about/AboutMeSection";
-import LandingTitle from "../components/landing/LandingTitle";
-import VideoBackground from "../components/landing/VideoBackground";
-import Menu from "../components/Menu/Menu";
+import LandingSection from "../components/landing/LandingSection";
 
 export default function Home() {
   return (
@@ -14,15 +12,9 @@ export default function Home() {
           content="Hey, I'm Anas Masti, I'm a Web developer"
         />
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
       </Head>
-
-      <VideoBackground />
-      <div className="h-screen flex justify-center items-center p-5 flex-col z-10">
-        <LandingTitle />
-        <div className="mt-4">
-          <Menu />
-        </div>
-      </div>
+      <LandingSection />
       <AboutMeSection />
     </main>
   );
