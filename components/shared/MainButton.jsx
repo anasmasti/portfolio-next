0ca7d0d7b4f605cd791688eb26c4f5bb1b597bd3
anasmasti <anasmasti@hotmail.com>
2022-12-22 09:49:18 +0100

    Fix UI

--- components/shared/MainButton.jsx
+++ components/shared/MainButton.jsx
@@ -1,8 +1,8 @@
-import Link from 'next/link'
-export default function MainButton({title, icon, goTo}) {
+import Link from "next/link";
+export default function MainButton({ title, icon, goTo }) {
   return (
     <Link href={goTo}>
-      <a className="p-2 bg-[#603cfe] rounded-xl text-white font-bold flex gap-1 transition-all duration-300 delay-75 hover:bg-black">
+      <a className="p-2 px-3 bg-[#603cfe] rounded-xl text-white font-bold flex gap-1 transition-all duration-300 delay-75 hover:bg-black">
         {title}
         {icon}
       </a>
